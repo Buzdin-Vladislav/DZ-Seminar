31972a7acfb4367492622d32cc60941b84fc8234
Buzdin vlad <vvavilon20@mail.ru>
2022-11-26 23:43:44 +0300

    domashnee zadanee 29

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿        // На Семинаре
+// Console.WriteLine("масив из N элементов");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string? [];
+
+// int count = 0;
+// int [] mass = new int [count];
+// for (int i=0; i < count; i++)
+// {
+//     mass[i] = new Random().Next(0,2);
+//     Console.WriteLine(mass [i]);
+// }
+//   От Алексея!
+//   for (int i=0; i<myArray.Length; i++)
+//   {
+//     myArray[i] = new Random().Next(0,30);
+//     // Console.Write(" "  +myArray[i]);
+//   }
+//   Console.Write('[' + string.Join(",",myArray) + ']');
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// int [] myArray = new int [] {1, 2, 5, 7, 19};
+// Console.Write('[' + string.Join(",",myArray) + ']');
+
+// int [] myArray = new int [] {6, 1, 33,};
+// Console.Write('[' + string.Join(",",myArray) + ']');
+// myArray[0] = 1;
+// myArray[1] = 2;
+// myArray[2] = 5;
+// myArray[3] = 7;
+// myArray[4] = 19;
+        
+        
+        // РЕШЕНИЕ!
+// int [] myArray = {1, 2, 5, 7, 19};
+//    for (int i = 0; i<myArray.Length; i++)
+//    {
+// Console.Write('[' + string.Join(",",myArray) + ']');
+//    }
+
+        // РЕШЕНИЕ!
+// int [] myArray = {6, 1, 33,};
+//    for (int i = 0; i<myArray.Length; i++)
+//    {
+// Console.Write('[' + string.Join(",",myArray) + ']');
+//    }
